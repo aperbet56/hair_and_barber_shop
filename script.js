af1b44d9798bcdffb200c0f4c8b20145112965aa
Annabelle Perbet <annabelleperbet@yahoo.fr>
2024-12-10 19:22:21 +0100

    Amélioration des commentaires

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ const navbar = document.querySelector(".navbar");
 const burger = document.querySelector(".burger");
 const navbarLinks = document.querySelectorAll(".navbar a");
 
-// Déclaration de la fonction toggleMenu qui va permettre d'afficher/ cacher le menu burger et lanavigation
+// Déclaration de la fonction toggleMenu qui va permettre d'afficher/ cacher le menu burger et la navigation
 const toggleMenu = () => {
   // Ecoute de l'événement "click" sur le menu burger
   burger.addEventListener("click", () => {
+    // Activation / désactivation de la classe show-nav
     navbar.classList.toggle("show-nav");
   });
 
   navbarLinks.forEach((link) => {
     // Ecoute de l'événement "click" sur le lien
     link.addEventListener("click", () => {
+      // Activation / désactivation de la classe show-nav
       navbar.classList.toggle("show-nav");
     });
   });
